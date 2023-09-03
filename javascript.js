61063aa36be46aef75178d1c4aba3674d15012ea
Darko Peshevski <darko.peshevski2@gmail.com>
2023-09-03 14:50:11 +0200

    Added function and modified exicting functions.

--- javascript.js
+++ javascript.js
@@ -1,40 +1,86 @@
+
 const mamaDiv = document.querySelector(".mama");
+const inputElement = document.querySelector(".fix-grid");
+const rowsContainer = document.createElement("div");
+const resetGridButton = document.querySelector(".reset-btn");
 
-// Creating the 16x16 grid here.
-for (let i = 16; i > 0; i--) {
-  const row = document.createElement('div');
-  row.className = 'row';
-  mamaDiv.appendChild(row);
+rowsContainer.className = 'rows-container';
+mamaDiv.appendChild(rowsContainer);
 
-  for (let b = 16; b > 0; b--) {
-    const cell = document.createElement('div');
-    cell.className = 'square';
-    row.appendChild(cell);
+// In this constant and event bellow, we capture what is written in the input field element, and when pressing "Enter", we are firing off some functions.
+let inputValue;
+
+inputElement.addEventListener('keydown', function(event) {
+
+  if (event.key === "Enter") {
+    inputValue = inputElement.value;
+    inputValue = parseInt(inputValue);
+
+    deleteGrid();
+    createGrid(inputValue, inputValue);
+    colorTheSquares();
   }
-};
-
-const allSquares = document.querySelectorAll('.square');
-let flag = false;
-
-// Pressing and coloring the squares.
-allSquares.forEach(square => {
-
-  square.addEventListener('mousedown', function() {
-    square.style.backgroundColor = 'black';
-    flag = true;
-  });
-
-  square.addEventListener('mousemove', function() {
-    if (flag === true) {
-      square.style.backgroundColor = 'black';
-    }
-  });
-
-  square.addEventListener('mouseup', function() {
-    flag = false;
-  });
-
 });
 
+// Creating the starting GRID here.
+createGrid(16, 16);
+
+// Color the squares of the starting grid.
+colorTheSquares();
+
+
+function createGrid(xSquares, ySquares) {
+  const totalWidth = 500;
+  let squareSize = totalWidth / xSquares;
+
+  for (let i = xSquares; i > 0; i--) {
+    const row = document.createElement('div');
+    row.className = 'row';
+    rowsContainer.appendChild(row);
+  
+    for (let b = ySquares; b > 0; b--) {
+      const cell = document.createElement('div');
+      cell.style.width = squareSize + 'px';
+      cell.style.height = squareSize + 'px';
+      cell.className = 'square';
+      row.appendChild(cell);
+    }
+  };
+}
+
+// Deleting the grid function.
+function deleteGrid() {
+  const allDivs = document.querySelectorAll('.row');
+  allDivs.forEach(div => {
+    div.remove();
+  });
+};
+
+
+// Function that colors the squares based on mouse events.
+function colorTheSquares() {
+
+  const allSquares = document.querySelectorAll('.square');
+  let flag = false;
+
+  allSquares.forEach(square => {
+
+    square.addEventListener('mousedown', function() {
+      square.style.backgroundColor = 'black';
+      flag = true;
+    });
+
+    square.addEventListener('mouseover', function() {
+      if (flag === true) {
+        square.style.backgroundColor = 'black';
+      }
+    });
+
+    square.addEventListener('mouseup', function() {
+      flag = false;
+    });
+
+  });
+};
 
 
