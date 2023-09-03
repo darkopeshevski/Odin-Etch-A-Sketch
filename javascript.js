26282c3567ed788834ff6098ef6c3d0a21f7ca2e
Darko Peshevski <darko.peshevski2@gmail.com>
2023-09-03 15:57:43 +0200

    Added a new button when pressed, it delets the color from the squares and resets the current grid.

--- javascript.js
+++ javascript.js
@@ -4,29 +4,51 @@ const inputElement = document.querySelector(".fix-grid");
 const rowsContainer = document.createElement("div");
 const resetGridButton = document.querySelector(".reset-btn");
 
+let inputValue = 0; // A constant to hold the input value.
+
 rowsContainer.className = 'rows-container';
 mamaDiv.appendChild(rowsContainer);
 
-// In this constant and event bellow, we capture what is written in the input field element, and when pressing "Enter", we are firing off some functions.
-let inputValue;
-
-inputElement.addEventListener('keydown', function(event) {
-
-  if (event.key === "Enter") {
-    inputValue = inputElement.value;
-    inputValue = parseInt(inputValue);
-
-    deleteGrid();
-    createGrid(inputValue, inputValue);
-    colorTheSquares();
-  }
-});
 
 // Creating the starting GRID here.
 createGrid(16, 16);
 
 // Color the squares of the starting grid.
 colorTheSquares();
+
+// Reset the grid and delete the colored squares.
+resetGridButton.addEventListener('click', function() {
+  deleteGrid();
+
+  if (inputValue === 0) {
+    createGrid(16, 16);
+    colorTheSquares();
+  }
+  else {
+    createGrid(inputValue, inputValue);
+    colorTheSquares();
+  }
+});
+
+// In the event bellow, we capture what is written in the input field element, and when pressing "Enter", we are firing off some functions.
+
+inputElement.addEventListener('keydown', function(event) {
+
+  if (event.key === "Enter") {
+    inputValue = parseInt(inputElement.value);
+    
+    if (inputValue >= 100) {
+      alert('Please insert a number less than 100!');
+      inputValue = 0;
+      inputElement.value = 0;
+    }
+    else {
+      deleteGrid();
+      createGrid(inputValue, inputValue);
+      colorTheSquares();
+    }
+  }
+});
 
 
 function createGrid(xSquares, ySquares) {
